395634f8f83925ecbd07f66639ad56502b8fce80
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-03-23 01:57:43 +0530

    created remove request

---
+++ delete.js
@@ -0,0 +1,78 @@
+
+// function validateAndGetFormData() {
+//   var recordnumberVar = $("#recordnumber").val();
+//   if (recordnumberVar === "") {
+//     alert("Record Number is Required");
+//     $("#recordnumber").focus();
+//     return "";
+//   }
+  
+  
+//   var jsonStrObj = JSON.stringify([new String('record'), new Number(recordnumberVar)]);
+//   return jsonStrObj;
+// }
+
+
+// This method is used to create PUT Json request.
+function createPUTRequest(connToken, recordnum, dbName, relName) {
+  var putRequest =
+    "{\n" +
+    '"token" : "' +
+    connToken +
+    '",' +
+    '"dbName": "' +
+    dbName +
+    '",\n' +
+    '"cmd" : "REMOVE",\n' +
+    '"rel" : "' +
+    relName +
+    '",' +
+    '"record": \n' +
+    recordnum +
+    "\n" +
+    "}";
+  return putRequest;
+}
+
+
+function executeCommand(reqString, dbBaseUrl, apiEndPointUrl) {
+  var url = dbBaseUrl + apiEndPointUrl;
+  var jsonObj;
+  $.post(url, reqString, function (result) {
+    jsonObj = JSON.parse(result);
+  }).fail(function (result) {
+    var dataJsonObj = result.responseText;
+    jsonObj = JSON.parse(dataJsonObj);
+  });
+  return jsonObj;
+}
+
+
+function resetForm() {
+  $("#recordnumber").val("");
+ 
+ 
+  $("#recordnumber").focus();
+}
+
+
+function deleteEmployee() {
+  var recordnum = $("#recordnumber").val(); 
+
+  var putReqStr = createPUTRequest(
+    "90938779|-31948831936850569|90945976",
+    recordnum,
+    "CRUD_Project_DB",
+    "user"
+  );
+  alert(putReqStr);
+  jQuery.ajaxSetup({ async: false });
+  var resultObj = executeCommand(
+    putReqStr,
+    "http://api.login2explore.com:5577",
+    "/api/iml"
+  );
+  alert(JSON.stringify(resultObj));
+  jQuery.ajaxSetup({ async: true });
+  resetForm();
+}
